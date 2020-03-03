919d35bf0c0c893c68e33340bb5477ba31e6a9d4
abdaloth <abdullahoalothman@gmail.com>
2020-03-03 16:25:07 -0500

    saved transfer model test results

--- transfer_cnn.py
+++ transfer_cnn.py
@@ -1,12 +1,15 @@
+# %%
+import numpy as np
 from keras.applications.vgg16 import VGG16
 
 from keras.models import Model
 from keras import layers as nn
-from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.callbacks import EarlyStopping
 
-from utils import make_submission, get_data, ROCAUC
+from utils import make_submission, get_data, plot_roc, plot_prediction_samples
 
-
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
 
 
 # %%
@@ -27,21 +30,27 @@ model.compile(loss='binary_crossentropy', optimizer='adam')
 model.summary()
 # %%
 X, y = get_data(as_gray=False)
+X_train, X_val, y_train, y_val = train_test_split(X, y,
+                                                  test_size=0.2,
+                                                  random_state=42,
+                                                  stratify=y)
 
 batch_size = 128
-ra = ROCAUC(batch_size)
-es = EarlyStopping(monitor='val_auc', patience=2, mode='max')
-mc = ModelCheckpoint(f'data/models/model.h5',
-                     monitor='val_auc',
-                     save_best_only=True,
-                     mode='max', verbose=1)
-model.fit(X, y,
+es = EarlyStopping(monitor='val_loss', patience=2, mode='min')
+
+model.fit(X_train, y_train,
           batch_size=batch_size,
-          epochs=50,
-          validation_split=.2,
-          callbacks=[ra, es, mc])
+          epochs=5,
+          validation_data=(X_val, y_val),
+          callbacks=[es])
 
 
+# %%
+prediction_scores = model.predict(X_val, batch_size=128)
+np.save('data/transfer_cnn_train_preds.npy',
+        prediction_scores.flatten())
+np.save('data/transfer_cnn_train_lbls.npy',
+        y_val)
 # %%
 model.fit(X, y, batch_size=batch_size, epochs=5)
 
@@ -51,5 +60,5 @@ X_test, test_ids = get_data(test=True, as_gray=False)
 
 test_predictions = model.predict(X_test, batch_size=batch_size)
 test_predictions = test_predictions.flatten()
-make_submission(test_ids, test_predictions, 'submissions/first_transfer_cnn.csv')
-
+make_submission(test_ids, test_predictions,
+                'submissions/first_transfer_cnn.csv')
